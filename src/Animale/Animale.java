2c38494990385a2795026a14c2347abdc6ceccd0
Cassia <cassia@MacBook-Pro-di-Alessandro.local>
2018-06-18 12:01:12 +0200

    first commit

---
+++ src/Animale/Animale.java
@@ -0,0 +1,17 @@
+package Animale;
+
+import java.util.Scanner;
+
+public class Animale {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println("Scrivi un nome");
+		String nome = scanner.nextLine();
+		System.out.println(nome + " è un animale");
+	}
+
+}
