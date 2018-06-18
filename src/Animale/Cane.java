6d8f9881836fe48b355f7f339975647969146b9e
studente <studente@DESKTOP-957MHLG>
2018-06-18 12:23:19 +0200

    nuova classe cane

---
+++ src/Animale/Cane.java
@@ -0,0 +1,21 @@
+package Animale;
+
+import java.util.Scanner;
+
+public class Cane {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println ("Scrivi il verso cane:");
+		
+		String verso = scanner.nextLine();
+		
+		System.out.println("Il verso del cane è: " + verso);
+		
+		
+	}
+
+}
